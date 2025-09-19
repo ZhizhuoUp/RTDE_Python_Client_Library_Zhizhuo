f0d820724de6e82b323a64497407eac461686183
Zhizhuo Zhang <zhizhuo0928@outlook.com>
2025-09-19 16:25:48 -0400

    2025/9/19 laptop update: add socket

---
+++ zzz_socket.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = '192.168.1.2'
+PORT = 30001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(((HOST, PORT)))
+
+cmd = "movej([0, 0.7, 0, 1.57, 0, 0], a=1.0, v=0.2)\n"
+print('send command!')
+s.sendall(cmd.encode("utf-8"))
+s.close()
